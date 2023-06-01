9bf5f42ebc0fcfa0a837d4b08fa5d111bebcfea9
wind14user <admin@wind14user.ru>
2023-06-01 21:37:24 +0300

    123

--- web_django/t/run.sh
+++ web_django/t/run.sh
@@ -7,5 +7,6 @@ fi
 trap "deactivate && echo virtual environment deactivated" SIGINT SIGTERM
 source venv/bin/activate && echo virtual environment activated
 pushd django_project
+django-admin --version
 ./manage.py runserver 0.0.0.0:8080
 #PLATFORM_BUILD_PATH=static_build && ./manage.py runserver 0.0.0.0:8080
